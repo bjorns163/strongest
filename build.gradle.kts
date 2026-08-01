fa7e9b06ff35c58f4485ca2a1147d57f155e543b
Bjorn Stout <bjorn.stout@hotmail.com>
2026-08-01 19:35:50 +0200

    bump versions and fix progress graphs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "9.3.1" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.4.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
     id("com.google.devtools.ksp") version "2.3.9" apply false
-    id("com.google.dagger.hilt.android") version "2.60" apply false
+    id("com.google.dagger.hilt.android") version "2.60.1" apply false
 }
